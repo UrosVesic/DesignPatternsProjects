1162e6a0beb4bca4b259b00a8395005b15faccde
UrosVesic <uros99uki@gmail.com>
2022-02-08 00:50:11 +0100

    first commit

---
+++ paterni-ponasanja/SKTM/src/Client/Kontroler.java
@@ -0,0 +1,27 @@
+
+
+package Client;
+
+import AbstractProductA.EkranskaForma;
+import AbstractProductB.BrokerBazePodataka;
+import DomainClasses.DKSkiKarta;
+import DomainClasses.GeneralDObject;
+
+public abstract class Kontroler {
+    protected EkranskaForma ef;
+    protected BrokerBazePodataka bbp;
+    DKSkiKarta sk;   // Promenljivo!!!
+    String poruka;
+    
+    public void napuniGrafickiObjekatIzDomenskogObjekta(GeneralDObject gdo) {}
+    public void napuniDomenskiObjekatIzGrafickogObjekta() {}
+    public void isprazniGrafickiObjekat(){}
+    public void setPoruka(String poruka1){poruka=poruka1;}
+    public void prikaziPoruku(){}
+    public void prikaziPoruku(String s){}
+    public  EkranskaForma getEkranskaForma(){return ef;}
+    public BrokerBazePodataka getBrokerBazePodataka(){return bbp;}
+    public GeneralDObject getDomenskiObjekat(){return sk;}
+    public void setDomenskiObjekat(GeneralDObject gdo){sk = (DKSkiKarta)gdo;}
+             
+}
---
+++ paterni-ponasanja/SKTM/src/ConcreteClass/KreirajDomenskiObjekat.java
@@ -0,0 +1,56 @@
+
+package ConcreteClass;
+
+import AbstractClass.OpstaDBSO;
+import AbstractProductB.BrokerBazePodataka;
+import Client.Kontroler;
+import DomainClasses.DKSkiKarta;
+import DomainClasses.GeneralDObject;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class KreirajDomenskiObjekat extends OpstaDBSO { // ConcreteClass
+
+    public KreirajDomenskiObjekat(BrokerBazePodataka bbp1, Kontroler kon1) {
+        super(bbp1, kon1);
+    }
+
+    @Override
+    public void makeConnection() {
+        bbp.makeConnection();
+    }
+
+    @Override
+    public boolean konkretnaDBSO() // insert record
+    {
+        kon.isprazniGrafickiObjekat();
+        GeneralDObject ip = kon.getDomenskiObjekat();
+        AtomicInteger counter = new AtomicInteger(0);
+
+        if (!bbp.getCounter(ip, counter)) {
+            return false;
+        }
+        if (!bbp.increaseCounter(ip, counter)) {
+            return false;
+        }
+
+        ((DKSkiKarta) ip).setSifraSkiKarte(counter.get()); // Promenljivo!!!
+
+        return bbp.insertRecord(ip);
+
+    }
+
+    @Override
+    public void uspesnaDBOperacija() {
+        bbp.commitTransation();
+        kon.setPoruka("Систем је креирао нову ски карту.");
+        kon.napuniGrafickiObjekatIzDomenskogObjekta(kon.getDomenskiObjekat());
+    }
+
+    @Override
+    public void neuspesnaDBOperacija() {
+        bbp.rollbackTransation();
+        kon.isprazniGrafickiObjekat();
+        kon.setPoruka("Систем не може да креира нову ски карту.");
+    }
+
+}
