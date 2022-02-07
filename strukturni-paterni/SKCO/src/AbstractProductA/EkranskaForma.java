a2507d3bf15c91d689f63ca5acde905ca164809f
UrosVesic <uros99uki@gmail.com>
2022-02-07 23:26:23 +0100

    first commit

---
+++ strukturni-paterni/SKCO/src/AbstractProductA/EkranskaForma.java
@@ -0,0 +1,18 @@
+
+
+package AbstractProductA;
+
+import Component.Komponenta;
+import javax.swing.JFrame;
+
+
+public abstract class EkranskaForma extends JFrame {
+    //Panel pan;
+    
+    public Panel getPanel() {return (Panel)getContentPane();}  
+    public void setPanel(Panel pan1) {setContentPane(pan1);//pan=pan1;
+        
+    }
+    public void prikaziEkranskuFormu(){setVisible(true);}
+    abstract public void odrediDekoracijuForme();
+}
